fc1ba9d7ac705c7195d6c1f9620b60aa67c36f42
JavL109 <jlopez21167@iesjoanramis.org>
2025-10-06 14:02:51 +0200

    Add files via upload

---
+++ Currículum/primero/script.js
@@ -0,0 +1,61 @@
+let visible = true;
+
+function camb_nomb(){
+    let nom = prompt("INTRODUCE UN NOMBRE: ");
+    document.getElementById("nom").innerHTML = nom;
+
+}
+
+function transform(){
+    let url = prompt("¿QUIÉN QUIERES SER?: ")
+    const image = document.getElementById("profile");
+    image.src = url;
+
+}
+
+function travel(){
+    const body = document.getElementsByTagName("body")[0];
+    let url2 = prompt("¿DÓNDE QUIERES VIAJAR?: ");
+    body.style = "background-image: url('"+url2+"'); background-size:cover;"
+}
+
+
+function learn(){
+    const body = document.getElementsByTagName("html")[0];
+    body.style.fontFamily = "fantasy"
+}
+
+function invisible(){
+    if (document.getElementById("profile").hidden == true){
+        document.getElementById("profile").hidden = false;
+    }else{
+        document.getElementById("profile").hidden = true;
+    }
+}
+
+function more_info(){
+    const expL = document.getElementById("expL");
+    const nothing = document.getElementById("nothing"); 
+    if (nothing){   //Si existe "nothing"
+        nothing.remove();  //Elimina el string "Nada" del apartado experiencia laboral
+    }
+    let content = prompt("¿QUÉ QUIERES AÑADIR?: ");
+    let lista = document.createElement("li");
+    lista.innerHTML = content;
+    expL.appendChild(lista);
+}
+
+function change_mode(){
+    window.location.href = "./../index2.html"
+}
+
+function square(){
+    const square = document.getElementById("rectangle");
+    if (visible){
+        square.style.display = "flex";
+        visible = false;
+    } else {
+        square.style.display = "none";
+        visible = true;
+    }
+}
